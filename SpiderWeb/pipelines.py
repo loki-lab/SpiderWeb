144204af72a72643a9f667dd8d1d4ddd8789f6be
loki-lab <nguyenvanminhcuong2002@gmail.com>
2023-12-24 21:38:43 +0700

    upload

--- SpiderWeb/pipelines.py
+++ SpiderWeb/pipelines.py
@@ -6,8 +6,32 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from decouple import config
+import pymongo
+import traceback
 
 
 class SpiderwebPipeline:
     def process_item(self, item, spider):
         return item
+    
+class MongoDBPipeline(object):
+    def __init__(self):
+        url = config("url")
+        clent = pymongo.MongoClient(url)
+        db = clent["chanhtuoi"]
+        self.db = db
+    
+    def process_item(self, item, spider):
+        try: 
+            parseItem = ItemAdapter(item).asdict()
+            existed_o = self.db.article_details_raw.find_one({"url": parseItem["url"]}, {"url":1})
+            if not existed_o:
+                self.db.article_details_raw.insert_one(parseItem)
+            else: 
+                print("Existed url: {}".format(parseItem["url"]))
+
+        except Exception as e: 
+            print("Error: db.article_details_raw.insert_one", e)
+            print(traceback.format_exc())
+        return item 
